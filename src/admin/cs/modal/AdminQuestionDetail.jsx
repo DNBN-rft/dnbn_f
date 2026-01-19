d40ec9c02d9da6e1f3c8f4e65b60633180a47ea1
Soyun_p <ehouse190@gmail.com>
2026-01-19 16:26:20 +0900

    custIdx -> custCode

--- src/admin/cs/modal/AdminQuestionDetail.jsx
+++ src/admin/cs/modal/AdminQuestionDetail.jsx
@@ -83,7 +83,7 @@ const AdminQuestionDetail = ({ questionIdx, onClose }) => {
   const getUserType = () => {
     if (!questionDetail) return "-";
     if (questionDetail.storeIdx) return "가맹점";
-    if (questionDetail.custIdx) return "일반사용자";
+    if (questionDetail.custCode) return "일반사용자";
     return "-";
   };
   if (loading) {
